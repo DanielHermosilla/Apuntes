29f6cd45eeacdacd5b9b2808f0c8a362872733c9
Daniel Hermosilla <daniel.hermosilla.r@ug.uchile.cl>
2023-02-23 20:50:30 -0300

    Backup: 2023-02-23 20:50:29

--- Programación/MergeSort.py
+++ Programación/MergeSort.py
@@ -6,9 +6,16 @@ def dividir(array):
     if largo == 0: 
         return array
     
+    izquierda = array[0:largo]
+    derecha = array[largo:]
+
     print(array)
-    return dividir(array[0:largo])
+
+    return dividir(izquierda) and dividir(derecha)
 
 lista = [1,2,3,4,5,6,7,8,9,10,11,12]
+
+def merge(array, izq, der): 
+    pass
 
 print(dividir(lista))
