3cea08b5f1705449cb46152c4cb61dfce61b1075
Daniel Hermosilla <daniel.hermosilla.r@ug.uchile.cl>
2023-02-23 22:32:45 -0300

    Backup: 2023-02-23 22:32:45

--- Programación/MergeSort.py
+++ Programación/MergeSort.py
@@ -1,21 +1,34 @@
 
+def swap(lst, i , j):
+
+	lst[i], lst[j] = lst[j], lst[i]
+        
 def dividir(array):
 
     largo = len(array)//2
-
-    if largo == 0: 
-        return array
-    
     izquierda = array[0:largo]
     derecha = array[largo:]
 
-    print(array)
-
+    if len(izquierda) == 1 or len(derecha) == 1:
+        return merge(array, izquierda, derecha)
+         
     return dividir(izquierda) and dividir(derecha)
 
-lista = [1,2,3,4,5,6,7,8,9,10,11,12]
+lista = [11,33,44,77,88,99,55,44,33]
 
 def merge(array, izq, der): 
-    pass
+
+    for i in range(0,min(len(izq),len(der)),2):
+
+        if izq[i] > der[i]:
+            array[i] = der[i]
+            array[i+1] = izq[i]
+
+        else: 
+            array[i] = izq[i]
+            array[i+1] = der[i]
+
+    return array
+
 
 print(dividir(lista))
