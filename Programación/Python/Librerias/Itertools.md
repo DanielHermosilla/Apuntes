5e0aea7fb7086153b8ecbac20b51262d62b973cc
Daniel Hermosilla <daniel.hermosilla.r@ug.uchile.cl>
2023-02-25 22:58:45 -0300

    Backup: 2023-02-25 22:58:45

---
+++ Programación/Python/Librerias/Itertools.md
@@ -0,0 +1,9 @@
+Es un [[Librerias|modulo]] nativo de Python que permite poder trabajar con [[Bucles|bucles]] e iteradores de una forma legible y expresible. 
+
+## Funciones 
+
+### Iteradores infinitos 
+
+- *Count* : Retorna un iterador infinito que comienza en *n*  y tiene paso *step*. Su sintaxis es count(n = 0, step = 1). 
+
+- *Cycle:* Retorna un iterador que itera infinitamente por el iterable suministrado como parámetro. 
--- Programación/QuickSort.py
+++ Programación/QuickSort.py
@@ -24,38 +24,53 @@ def QuickSort(array):
 
 array = [55,66,22,33,99,88,11,44]
 
+
+# Función que cambia de lugar elementos de la lista 
+
 def swap(lst, i , j):
-    lst[i], lst[j] = lst[j], lst[i]
-    
 
-def QuickSort2(array): 
-
-    largo = len(array)
-
-    if largo == 0:
-        return []
-    
-    pivote = array.pop()
-
-    menores = list()
-    mayores = list()
-
-    for i in range(-1, largo-1, 1):
+	lst[i], lst[j] = lst[j], lst[i]
 	
-	    for j in range(largo-1):
+# Algoritmo QuickSort optimizado 
+
+def QuickSort2(array):
+
+	largo = len(array)
+	
+	if largo == 0:
+	
+		return []
+
+	pivote = array.pop()
+	
+	menores = list()
+	
+	mayores = list()
+	
+	for i in range(-1, largo-1, 1):
+	
+		for j in range(largo-1):
 		
-		    if array[j] > pivote:
-                pass
-            
-		    else:
+			if array[j] > pivote:
+				None
+
+			else:
 				i += 1
 				swap(array, i, j)
-			 
-		 array.insert(pivote, i + 1)
-		menores = array[0:i]
+
+		
+		array.insert(i+1, pivote)
+		menores = array[0:i+1]
 		mayores = array[i+2:]
 		break
 
-    return QuickSort(menores) + [pivote] + QuickSort(mayores)
+	return QuickSort(menores) + [pivote] + QuickSort(mayores)
+
+# TEST 
+
+array = [55,66,22,33,99,88,11,44]
+
+print(QuickSort(array))
+
 
 print(QuickSort2(array))
