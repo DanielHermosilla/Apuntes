bf599e30e5c853727c920cdf7796094e7502691d
Daniel Hermosilla <daniel.hermosilla.r@ug.uchile.cl>
2023-03-06 13:19:10 -0300

    Añadir código de Introducción

--- Programación/Python/Data Science from Scratch/Introduccion.py
+++ Programación/Python/Data Science from Scratch/Introduccion.py
@@ -90,11 +90,35 @@ def not_friends(user, other_user):
 
 def friends_of_friend_ids(user):
     return Counter(foaf["id"]
-                   for friend in user["friends"]  # for each of my friends
-                   for foaf in friend["friends"]  # count *their* friends
-                   if not_the_same(user, foaf)    # who aren't me
-                   and not_friends(user, foaf))   # and aren't my friends
+                   for friend in user["friends"]  # Por cada uno de mis amigos
+                   for foaf in friend["friends"]  # Contar a cada amigo
+                   if not_the_same(user, foaf)    # Que no sean "yo"
+                   and not_friends(user, foaf))   # Y que no sean mis amigos
 
-userX = users[3]
-print(friends_of_friend_ids(userX))
+
+# Se compila una lista de intereses de cada usuario 
+
+interests = [
+    (0, "Hadoop"), (0, "Big Data"), (0, "HBase"), (0, "Java"),
+    (0, "Spark"), (0, "Storm"), (0, "Cassandra"),
+    (1, "NoSQL"), (1, "MongoDB"), (1, "Cassandra"), (1, "HBase"),
+    (1, "Postgres"), (2, "Python"), (2, "scikit-learn"), (2, "scipy"),
+    (2, "numpy"), (2, "statsmodels"), (2, "pandas"), (3, "R"), (3, "Python"),
+    (3, "statistics"), (3, "regression"), (3, "probability"),
+    (4, "machine learning"), (4, "regression"), (4, "decision trees"),
+    (4, "libsvm"), (5, "Python"), (5, "R"), (5, "Java"), (5, "C++"),
+    (5, "Haskell"), (5, "programming languages"), (6, "statistics"),
+    (6, "probability"), (6, "mathematics"), (6, "theory"),
+    (7, "machine learning"), (7, "scikit-learn"), (7, "Mahout"),
+    (7, "neural networks"), (8, "neural networks"), (8, "deep learning"),
+    (8, "Big Data"), (8, "artificial intelligence"), (9, "Hadoop"),
+    (9, "Java"), (9, "MapReduce"), (9, "Big Data")
+]
+
+# Función de gente con intereses similares 
+
+def data_scientist_who_like(target_interest): 
+    return [user_id
+            for user_id, user_interest in interests
+            if user_interest == target_interest]
 
